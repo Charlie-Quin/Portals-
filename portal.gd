ddd89dc59b168c96426b6b7856dbaf55f0a3f289
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2024-05-03 18:44:59 -0400

    frameworking

--- portal.gd
+++ portal.gd
@@ -2,11 +2,38 @@ extends Node3D
 
 @export var targetPortal : Node3D
 
+@onready var stuff : Node3D = $SubViewport/stuff
+@onready var camera : Camera3D = $SubViewport/stuff/Camera3D
+
+var player : CharacterBody3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if not player:
+		player = Globals.player
+		if not player:
+			return
+	
+	var cameraOffset = stuff.global_position - player.global_position
+	
+	camera.global_position = targetPortal.stuff.global_position + cameraOffset
+	
 	pass
+
+
+
+
+
+
+
+
+
