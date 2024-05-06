d36da66c1bf866f1345925922500531733f73788
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2024-05-06 09:05:58 -0400

    Revert "it does not play nice with vr"
    This reverts commit c4e17559b0e99dc5053b8abb566a6d7c3f12a113.

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -3,19 +3,3 @@ extends Node
 var player : CharacterBody3D
 
 var VRCAMERA : XRCamera3D
-
-var interface = XRInterface
-var initializedVR = false
-func _ready():
-	
-	if initializedVR:
-		return
-	initializedVR = true
-	
-	interface = XRServer.find_interface("OpenXR")
-	
-	if interface and interface.is_initialized():
-		print("yipee")
-		get_viewport().use_xr = true
-	
-	pass
